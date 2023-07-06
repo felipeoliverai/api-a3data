3981468cb980778444c66cfa46feee24e32d6c57
Felipe <felipe.oliveiras2000@gmail.com>
2023-07-06 17:01:38 -0300

    api

--- Dockerfile
+++ Dockerfile
@@ -7,25 +7,25 @@ WORKDIR /app
 # copiar os arquivos necessários para o diretório de trabalho
 COPY . /app
 
-# Instalar as dependências
+# instalar as dependências
 RUN pip install --upgrade pip 
 RUN pip install -r requirements.txt
 
-# Definir a variável de ambiente para o caminho do arquivo de logs
+# variável de ambiente para o caminho do arquivo de logs
 ENV LOG_FILE_PATH /app/api_logs.log
 
-# Copiar o arquivo openssl.cnf para o diretório apropriado dentro do contêiner
+# copiar o arquivo openssl.cnf para o diretório dentro do contêiner
 COPY openssl.cnf /etc/ssl/openssl.cnf
 
-# Definir a variável de ambiente OPENSSL_CONF com o caminho correto
+# variável de ambiente OPENSSL_CONF
 ENV OPENSSL_CONF /etc/ssl/openssl.cnf
 
-# Instalar o servidor WSGI (Gunicorn)
+# instalar o servidor WSGI (Gunicorn)
 RUN pip install gunicorn
 
-# Expor a porta em que a API vai rodar
+# expor a porta em que a API vai rodar
 EXPOSE 5000
 
-# Comando para iniciar a aplicação usando o Gunicorn
+# iniciar a aplicação usando o Gunicorn
 CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "api:app"]
 
--- api.py
+++ api.py
@@ -11,26 +11,27 @@ logging.basicConfig(filename='api_logs.log', level=logging.INFO)
 
 @app.route('/api_ibge', methods=['GET'])
 def api_ibge():
-    start_time = time.time()  # Tempo de início da execução
+    start_time = time.time()  # tempo de início da execução
 
+    # parametros 
     uf = request.args.get('uf')
     info = request.args.get('info')
 
     # URL da API
     url = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{uf}/distritos"
 
-    # Fazendo a chamada GET na API
+    # chamada GET na API IBGE
     response = requests.get(url)
     data = response.json()
 
     if data:
-        distrito = random.choice(data)  # Selecionando um registro aleatório
+        distrito = random.choice(data)  # selecionando um registro aleatório
         microrregiao = distrito["municipio"]["microrregiao"]["nome"]
         mesorregiao = distrito["municipio"]["microrregiao"]["mesorregiao"]["nome"]
         municipio = distrito["municipio"]["nome"]
         distrito_nome = distrito["nome"]
 
-        # Verificando a informação desejada
+        # verificando a informação desejada
         result = {}
         if info == 'microrregioes':
             result = {"microrregioes": [microrregiao]}
@@ -43,16 +44,16 @@ def api_ibge():
         else:
             return jsonify({"message": "Informação inválida. Escolha entre microrregioes, mesorregioes, municipios ou distritos."})
 
-        # Adicionando a UF ao resultado
+        # adicionando a UF ao resultado
         result["UF"] = uf
 
-        end_time = time.time()  # Tempo de término da execução
-        execution_time = (end_time - start_time) * 1000  # Tempo de execução em milissegundos
+        end_time = time.time()  # tempo de término da execução
+        execution_time = (end_time - start_time) * 1000  # tempo de execução em milissegundos
 
-        # Registro da requisição e tempo de execução no arquivo de logs
+        # registro da requisição e tempo de execução no arquivo de logs
         logging.info(f"Requisição - UF: {uf}, Info: {info}, Tempo de execução: {execution_time} ms")
 
-        # Retornando a resposta como JSON
+        # retornando a resposta como JSON
         return jsonify(result)
     else:
         return jsonify({"message": "Nenhum distrito encontrado para a UF especificada."})
