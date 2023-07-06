929a1604cc59861a16e2cd43bb61c0c3818efeb5
Felipe Oliveira <58439493+felipeoliverai@users.noreply.github.com>
2023-07-06 17:26:44 -0300

    Update api.py

--- api.py
+++ api.py
@@ -2,36 +2,37 @@ import logging
 import requests
 import random
 import time
+from datetime import datetime
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# configuração do logger
+# Configuração do logger
 logging.basicConfig(filename='api_logs.log', level=logging.INFO)
 
 @app.route('/api_ibge', methods=['GET'])
 def api_ibge():
-    start_time = time.time()  # tempo de início da execução
+    start_time = time.time()  # Tempo de início da execução
 
-    # parametros 
+    # Parâmetros
     uf = request.args.get('uf')
     info = request.args.get('info')
 
     # URL da API
     url = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{uf}/distritos"
 
-    # chamada GET na API IBGE
+    # Chamada GET na API IBGE
     response = requests.get(url)
     data = response.json()
 
     if data:
-        distrito = random.choice(data)  # selecionando um registro aleatório
+        distrito = random.choice(data)  # Selecionando um registro aleatório
         microrregiao = distrito["municipio"]["microrregiao"]["nome"]
         mesorregiao = distrito["municipio"]["microrregiao"]["mesorregiao"]["nome"]
         municipio = distrito["municipio"]["nome"]
         distrito_nome = distrito["nome"]
 
-        # verificando a informação desejada
+        # Verificando a informação desejada
         result = {}
         if info == 'microrregioes':
             result = {"microrregioes": [microrregiao]}
@@ -44,16 +45,20 @@ def api_ibge():
         else:
             return jsonify({"message": "Informação inválida. Escolha entre microrregioes, mesorregioes, municipios ou distritos."})
 
-        # adicionando a UF ao resultado
+        # Adicionando a UF ao resultado
         result["UF"] = uf
 
-        end_time = time.time()  # tempo de término da execução
-        execution_time = (end_time - start_time) * 1000  # tempo de execução em milissegundos
+        end_time = time.time()  # Tempo de término da execução
+        execution_time = (end_time - start_time) * 1000  # Tempo de execução em milissegundos
 
-        # registro da requisição e tempo de execução no arquivo de logs
-        logging.info(f"Requisição - UF: {uf}, Info: {info}, Tempo de execução: {execution_time} ms")
+        # Obtendo a data e hora atual
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # retornando a resposta como JSON
+        # Registro da requisição, tempo de execução e horário no arquivo de logs
+        log_message = f"{now} - Requisição - UF: {uf}, Info: {info}, Tempo de execução: {execution_time} ms"
+        logging.info(log_message)
+
+        # Retornando a resposta como JSON
         return jsonify(result)
     else:
         return jsonify({"message": "Nenhum distrito encontrado para a UF especificada."})
